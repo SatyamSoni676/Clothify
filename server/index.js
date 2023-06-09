e0a6fe9b66e0af00d81e3245f4772a138f82d35a
Satyam Soni <107421155+SatyamSoni676@users.noreply.github.com>
2023-06-10 00:24:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,13 +4,11 @@ import cors from 'cors';
 
 import dalleRoutes from './routes/dalle.routes.js';
 dotenv.config();
-
 const app = express();
 app.use(cors());
 app.use(express.json({ limig: "50mb" }))
 
 app.use("/api/v1/dalle", dalleRoutes);
-
 app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" })
 })
